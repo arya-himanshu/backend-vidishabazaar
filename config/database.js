bb6d79ed013052dbe3432a9318226d737ad9512d
HIMANSHU <himanshu@HIMANSHUs-MacBook-Air.local>
2022-12-31 13:16:39 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import { get } from "./env.js";
 
 get();
 export const mongodb = {
-  uri: `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/vidishabazaaDb`,
+  uri: `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}`,
   username: process.env.MONGO_DB_USERNAME,
   password: process.env.MONGO_DB_PASSWORD,
 };
